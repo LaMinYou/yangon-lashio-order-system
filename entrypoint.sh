49e9f0d5f2a2fd140b2e1217039be9b5db3815f6
AungSannOoDe <theflash1321@gmail.com>
2026-03-26 19:57:40 +0630

    fix:seed error

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 set -e
 
 echo "Waiting for database to be ready..."
-
+php artisan optimize
 # Loop until connection to DB_HOST on DB_PORT is successful
 # We use the env variables Laravel already uses
 while ! nc -z $DB_HOST ${DB_PORT:-3306}; do
@@ -12,7 +12,8 @@ while ! nc -z $DB_HOST ${DB_PORT:-3306}; do
 done
 
 echo "Database is up! Running migrations..."
-php artisan migrate --force
+
+php artisan migrate --force  --seed
 
 echo "Starting Laravel server..."
 exec php artisan serve --host=0.0.0.0 --port=8000
